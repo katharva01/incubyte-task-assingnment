8a6513355ae7bd4ec54fea5b7ae8b79204759519
Atharva <katharva01@gmail.com>
2023-08-26 00:14:35 +0530

    test : write test to check creation of Spacecraft

---
+++ Spacecraft.test.js
@@ -0,0 +1,18 @@
+
+
+
+describe('Spacecraft working check',()=>{
+    
+    let spacecraft;
+
+    beforeEach(() => {
+        spacecraft = new Spacecraft(0, 0, 0, "N");
+    });
+
+    it("should crete a spacecraft object with 'SpaceCraft' constructor",()=>{
+        expect(spacecraft.x).toBe(0);
+        expect(spacecraft.y).toBe(0);
+        expect(spacecraft.z).toBe(0);
+        expect(spacecraft.direction).toBe('N');
+    })
+});
